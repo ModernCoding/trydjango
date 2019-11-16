267da7688bebb83ad02e287504fc422483c84a3d
Cormorant Programming <cormorant.programming@gmail.com>
2019-11-16 18:21:04 +0700

    Loop

--- src/pages/views.py
+++ src/pages/views.py
@@ -16,7 +16,9 @@ def about_view(request, *args, **kwargs):
   # return HttpResponse("<h1>About us</h1>")
   my_context = {
     "my_text": "This is about me",
-    "my_number": 123
+    "this_is_true": True,
+    "my_number": 123,
+    "my_list": [3, 33, 43, "Abc"]
   }
   
   return render(request, "about.html", my_context)
