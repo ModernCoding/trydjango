12e5a6351bbacdba73f3f2f417178ddc6f8b2ddb
Cormorant Programming <cormorant.programming@gmail.com>
2019-11-17 09:33:38 +0700

    Forms

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 
 # Create your views here.
@@ -22,14 +22,50 @@ def product_detail_view(request):
 
 def product_create_view(request):
 
-  form = ProductForm(request.POST or None)
+  form = RawProductForm()
+  
+  if request.method == "POST":
+    form = RawProductForm(request.POST)
 
-  if form.is_valid():
-    form.save()
-    form = ProductForm()
+    if form.is_valid():
+      print(form.cleaned_data)
+      Product.objects.create(**form.cleaned_data)
+
+    # else:
+    #   print(form.errors)
+
 
   return render(
       request,
       "products/product_create.html",
       { "form": form }
     )
+
+
+# def product_create_view(request):
+#   print(request.GET)
+#   print(request.POST)
+
+#   if request.method == "POST":
+#     print(request.POST.get('title'))
+
+#   return render(
+#       request,
+#       "products/product_create.html",
+#       {}
+#     )
+
+
+# def product_create_view(request):
+
+#   form = ProductForm(request.POST or None)
+
+#   if form.is_valid():
+#     form.save()
+#     form = ProductForm()
+
+#   return render(
+#       request,
+#       "products/product_create.html",
+#       { "form": form }
+#     )
