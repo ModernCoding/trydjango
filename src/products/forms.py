a72d52ceb35fccce9cb6ddd1eb7c34b14e19a345
Cormorant Programming <cormorant.programming@gmail.com>
2019-11-19 15:55:19 +0700

    Form widgets

--- src/products/forms.py
+++ src/products/forms.py
@@ -15,6 +15,22 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-  title         = forms.CharField()
-  description   = forms.CharField()
-  price         = forms.DecimalField()
+  title         = forms.CharField(
+      label="Product name",
+      widget=forms.TextInput(attrs={
+          "placeholder": "Your title"
+        })
+    )
+  
+  description   = forms.CharField(
+      required=False,
+      widget=forms.Textarea(attrs={
+          "placeholder": "Your description",
+          "class": "new-class-name two",
+          "id": "my-id-for-textarea",
+          "rows": 19,
+          "cols": 40
+        })
+    )
+  
+  price         = forms.DecimalField(initial=4.19)
