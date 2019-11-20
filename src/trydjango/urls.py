f0e9833ada98f723dd7031f74ce2d8b7aedb9cf5
Cormorant Programming <cormorant.programming@gmail.com>
2019-11-20 18:16:32 +0700

    ListView

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
   path('about/', about_view),
   path('social/', social_view),
 
-  path('products/', include('products.urls'))
+  path('products/', include('products.urls')),
+  path('blog/', include('blog.urls'))
 ]
